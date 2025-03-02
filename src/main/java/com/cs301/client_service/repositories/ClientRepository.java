702b492ff95fec2b4f02954c1bd2108277fffcd8
jeremainecheong <132808191+jeremainecheong@users.noreply.github.com>
2025-03-03 00:19:39 +0800

    add: agentid query

--- src/main/java/com/cs301/client_service/repositories/ClientRepository.java
+++ src/main/java/com/cs301/client_service/repositories/ClientRepository.java
@@ -3,5 +3,8 @@ package com.cs301.client_service.repositories;
 import com.cs301.client_service.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClientRepository extends JpaRepository<Client, String> {
+    List<Client> findByAgentId(String agentId);
 }
