a06bae8698c700447182cba4d8ef6e5fdd567d77
jeremainecheong <132808191+jeremainecheong@users.noreply.github.com>
2025-03-03 00:14:21 +0800

    fix: static sql query

--- src/main/java/com/cs301/client_service/repositories/AccountRepository.java
+++ src/main/java/com/cs301/client_service/repositories/AccountRepository.java
@@ -2,11 +2,19 @@ package com.cs301.client_service.repositories;
 
 import com.cs301.client_service.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
     List<Account> findByClientClientId(String clientId);
-    void deleteByClientClientId(String clientId);
+    
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Account a WHERE a.client.clientId = :clientId")
+    void deleteByClientClientId(@Param("clientId") String clientId);
 }
