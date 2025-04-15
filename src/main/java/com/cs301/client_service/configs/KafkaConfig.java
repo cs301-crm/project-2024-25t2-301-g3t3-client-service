4690e6143aca1171a22c5b4e87a4d8d9fef7e37e
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-04-16 00:49:10 +0800

    feat: fix

--- src/main/java/com/cs301/client_service/configs/KafkaConfig.java
+++ src/main/java/com/cs301/client_service/configs/KafkaConfig.java
@@ -1,7 +1,5 @@
 package com.cs301.client_service.configs;
 
-import com.amazonaws.services.schemaregistry.serializers.GlueSchemaRegistryKafkaSerializer;
-import com.amazonaws.services.schemaregistry.utils.AWSSchemaRegistryConstants;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,8 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.glue.model.DataFormat;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,16 +25,20 @@ public class KafkaConfig {
      * Creates a producer factory for Kafka
      * @return the configured producer factory
      */
+    /**
+     * Creates a producer factory for Kafka
+     * @return the configured producer factory
+     */
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GlueSchemaRegistryKafkaSerializer.class.getName());
-        configProps.put(AWSSchemaRegistryConstants.AWS_REGION, Region.AP_SOUTHEAST_1);
-        configProps.put(AWSSchemaRegistryConstants.DATA_FORMAT, DataFormat.PROTOBUF.name());
-        configProps.put(AWSSchemaRegistryConstants.REGISTRY_NAME, schemaRegistryUrl);
-        
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class);
+        configProps.put("schema.registry.url", schemaRegistryUrl);
+        configProps.put("auto.register.schemas", true);
+        configProps.put("use.latest.version", true);
+
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
